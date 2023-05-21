88c3e652f2ac7fd14c9bc142a850aa119d104255
Alex Scotton (Illizian) <alex@alexscotton.com>
2023-05-21 21:01:41 +0100

    Adds nextra template

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>TechEducators_</span>,
+  project: {
+    link: 'https://techeducators.co.uk/',
+  },
+  chat: {
+    link: 'https://discord.gg/zEeTnhfzdt',
+  },
+  docsRepositoryBase: 'https://github.com/TechEducatorsTeam/development-setup',
+  footer: {
+    text: 'TechEducators_',
+  },
+}
+
+export default config
